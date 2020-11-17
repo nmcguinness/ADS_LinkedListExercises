0ae207cb7ae10fbe47d22bc600b46abc0ebd5b2f
niall mcguinness <niall.mcguinness@dkit.ie>
2020-11-17 15:17:42 +0000

    Formatted main cpp file with comments

--- ADS_LinkedListExercises/Main.cpp
+++ ADS_LinkedListExercises/Main.cpp
@@ -1,9 +1,25 @@
-// ADS_LinkedListExercises.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+/*****************************************************************//**
+ * \file   Main.cpp
+ * \brief  Solutions to linked-list exercises
+ * \see	   https://www.tutorialspoint.com/data_structures_algorithms/doubly_linked_list_algorithm.htm
+ * \author NMCG
+ * \date   November 2020
+ *********************************************************************/
 #include <iostream>
+using namespace std;
+
+//declarations
+void exercise1();
 
 int main()
 {
-	std::cout << "Get started...\n";
+	cout << "Exercises 1..." << endl;
+	exercise1();
+}
+
+/************************** Linked List Exercises **************************/
+
+//definitions
+void exercise1()
+{
 }
