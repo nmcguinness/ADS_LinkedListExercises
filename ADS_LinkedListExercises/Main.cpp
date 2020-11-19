87167e28c1147bb06d52d6cee7194cdbd2caf009
niall mcguinness <niall.mcguinness@dkit.ie>
2020-11-19 11:06:14 +0000

    Updated Main

--- ADS_LinkedListExercises/Main.cpp
+++ ADS_LinkedListExercises/Main.cpp
@@ -10,8 +10,12 @@ using namespace std;
 
 //declarations
 void exercise1();
+void exercise2();
 void exercise3();
 void exercise4();
+void exercise5();
+void exercise6();
+void exercise7();
 
 int main()
 {
@@ -26,27 +30,32 @@ void exercise1()
 {
 }
 
-/// @brief Implement a playlist for a Digital audio player
+/// @brief Modify the Doubly linked list to be a circular linked list.
+void exercise2()
+{
+}
+
+/// @brief Using your Circular linked list, implement a playlist for a Digital audio player.
 void exercise3()
 {
 }
 
-/// @brief Ask a user to input three numbers and print them out in reverse order.
+/// @brief Using your doubly linked list, write an application that asks a user to input three numbers and print them out in reverse order
 void exercise4()
 {
 }
 
-/// @brief Add 10 values to a linked list remove all odd numbers and display it.
+/// @brief Write an application to add 10 values to a linked list remove all odd numbers and display it.
 void exercise5()
 {
 }
 
-/// @brief Insert an element into the first position of an array
+/// @brief Write an algorithm to insert an element into the first position of an array
 void exercise6()
 {
 }
 
-/// @brief Measure how long it takes to add 10, 100, 1,000, 10,000 and 100, 000 elements into this array
+/// @brief Using the algorithm in five, measure how long it takes to add 10, 100, 1,000, 10,000 and 100, 000 elements into this array.Compare this performance to adding these elements to the head of your linked list.
 void exercise7()
 {
 }
